6360329f7e8a51f576249035bb9bd4f90fb69602
rshuken <ryanshuken@gmail.com>
2021-01-13 17:31:54 -0700

    deployment v4

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
   API_ENDPOINT:
-    'https://ryans-spaced-repetition-server.herokuapp.com',
+    'https://ryans-spaced-repetition-server.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 };
 
 
-//'https://cors-anywhere.herokuapp.com/https://ryans-spaced-repetition-server.herokuapp.com' ||g
+//'https://cors-anywhere.herokuapp.com/https://ryans-spaced-repetition-server.herokuapp.com' ||
